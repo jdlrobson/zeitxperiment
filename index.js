ae913328da22c122edefd75e507d3414bf3dd0f5
jdlrobson <jdlrobson@gmail.com>
2018-12-28 14:58:15 -0800

    something something

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const app = require('express')()
 app.get('/how', (req, res) => {
     res.send('How?');
 })
+app.get('/destination/:title', (req, res) => {
+    res.send(req.params.title);
+})
 app.get('*', (req, res) => {
   res.send('Hello from Express.js!');
 })
