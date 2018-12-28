ee973fcb64a6079c460043ac8d809cf26d73507e
jdlrobson <jdlrobson@gmail.com>
2018-12-28 15:46:47 -0800

    how about?

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-require( 'babel-core/register' );
-const app = require( './main.js' );
-app.listen();
+const express = require('express');
+const app = express();
+const fetch = require('node-fetch');
+
+function proxyFetch( res, path, type ) {
+    return fetch( `https://nomad.wmflabs.org${path}` ).then((resp) => {
+        res.status(resp.status);
+        return resp.text();
+    }).then((text) => {
+      res.setHeader( 'Content-Type', type );
+      res.send(text);
+    });
+}
+
+app.use( '/', express.static( __dirname + '/public' ) );
+app.get('/main.css', (req, res) => {
+    proxyFetch( res, req.url, 'text/css' );
+});
+app.get('/sw-bundle.js', (req, res) => {
+    proxyFetch( res, req.url, 'application/javascript' );
+});
+app.get('/main-bundle.js', (req, res) => {
+    proxyFetch( res, req.url, 'application/javascript' );
+});
+app.get('/api/*', (req, res) => {
+    proxyFetch( res, req.url, 'application/json' );
+});
+app.get('*', (req, res) => {
+  proxyFetch( res, req.params[0], 'text/html' );
+});
+app.listen(8100);
